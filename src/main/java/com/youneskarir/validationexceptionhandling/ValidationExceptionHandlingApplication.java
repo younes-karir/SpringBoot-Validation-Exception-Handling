81dfe6743b4c32a49e5bcfa67ffd5f090219b501
younes-karir <youneskarir@outlook.com>
2024-02-09 15:36:13 +0100

    first commit

---
+++ src/main/java/com/youneskarir/validationexceptionhandling/ValidationExceptionHandlingApplication.java
@@ -0,0 +1,13 @@
+package com.youneskarir.validationexceptionhandling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidationExceptionHandlingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidationExceptionHandlingApplication.class, args);
+    }
+
+}
