935d7821c94ed9254e1fe4b9c16e88426d5bfb68
younes-karir <youneskarir@outlook.com>
2024-02-11 13:51:19 +0100

    add the validation annotations

--- src/main/java/com/youneskarir/validationexceptionhandling/dto/UserRequest.java
+++ src/main/java/com/youneskarir/validationexceptionhandling/dto/UserRequest.java
@@ -1,6 +1,7 @@
 package com.youneskarir.validationexceptionhandling.dto;
 
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,9 +12,14 @@ import lombok.NoArgsConstructor;
 
 public class UserRequest {
 
+    @NotNull(message = "user name shouldn't be null")
     private String name;
+    @NotNull(message = "user address shouldn't be null")
     private String address;
+    @Email(message = "this is not a correct email id")
     private String email;
+    @Min(value = 18,message = "the age is under 18")
+    @Max(value = 65,message = "the age is over 65")
     private int age;
     private double salary;
 }
